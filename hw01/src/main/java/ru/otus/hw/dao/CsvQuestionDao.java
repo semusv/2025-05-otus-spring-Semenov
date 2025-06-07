83a406db9e0ad15da73c12e5297c904c1b84575b
Semenov V <semvasek@mail.ru>
2025-06-07 14:05:16 +0300

    The code is almost ready.

--- hw01/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
+++ hw01/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
@@ -1,14 +1,26 @@
 package ru.otus.hw.dao;
 
+import com.opencsv.bean.CsvToBeanBuilder;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.ClassPathResource;
 import ru.otus.hw.config.TestFileNameProvider;
+import ru.otus.hw.dao.dto.QuestionDto;
 import ru.otus.hw.domain.Question;
+import ru.otus.hw.exceptions.QuestionReadException;
 
-import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 import java.util.List;
 
 @RequiredArgsConstructor
 public class CsvQuestionDao implements QuestionDao {
+    public static final int SKIP_LINES = 1;
+
+    public static final char SEPARATOR = ';';
+
     private final TestFileNameProvider fileNameProvider;
 
     @Override
@@ -17,7 +29,34 @@ public class CsvQuestionDao implements QuestionDao {
         // https://opencsv.sourceforge.net/#collection_based_bean_fields_one_to_many_mappings
         // Использовать QuestionReadException
         // Про ресурсы: https://mkyong.com/java/java-read-a-file-from-resources-folder/
-
-        return new ArrayList<>();
+        try (InputStream inputStream = getResourceInputStream();
+             BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
+            var csvToBean = new CsvToBeanBuilder<QuestionDto>(reader)
+                    .withSkipLines(SKIP_LINES)
+                    .withSeparator(SEPARATOR)
+                    .withType(QuestionDto.class)
+                    .withOrderedResults(true)
+                    .build();
+            List<QuestionDto> questionDtos = csvToBean.stream().toList();
+            return questionDtos.stream().map(QuestionDto::toDomainObject).toList();
+        } catch (IOException e) {
+            throw new QuestionReadException("Error working with CSV file", e);
+        } catch (RuntimeException e) {
+            throw new QuestionReadException("Error parsing CSV file", e);
+        }
     }
+
+    private InputStream getResourceInputStream() throws IOException {
+        String fileName = fileNameProvider.getTestFileName();
+        ClassPathResource resource = new ClassPathResource(fileName);
+        InputStream inputStream;
+        try {
+            inputStream = resource.getInputStream();
+        } catch (IOException e) {
+            throw new IOException("Can't start read file with questions: " + fileName, e);
+        }
+        return inputStream;
+    }
+
+
 }
--- hw01/src/main/java/ru/otus/hw/service/TestServiceImpl.java
+++ hw01/src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -1,16 +1,55 @@
 package ru.otus.hw.service;
 
 import lombok.RequiredArgsConstructor;
+import ru.otus.hw.dao.QuestionDao;
+import ru.otus.hw.domain.Question;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 public class TestServiceImpl implements TestService {
 
+    private static final String ANSWER_SPACES = "    ";
+
     private final IOService ioService;
+
+    private final QuestionDao questionDao;
 
     @Override
     public void executeTest() {
         ioService.printLine("");
         ioService.printFormattedLine("Please answer the questions below%n");
-        // Получить вопросы из дао и вывести их с вариантами ответов
+
+        printFullQuestions(questionDao.findAll());
+    }
+
+    private void printFullQuestions(List<Question> questionList) {
+        int questionNumber = 0;
+
+        if (questionList == null || questionList.isEmpty()) {
+            throw new RuntimeException("No questions found");
+        }
+
+        for (var q : questionList) {
+            printOneQuestionAndAnswers(q, ++questionNumber);
+        }
+    }
+
+    private void printOneQuestionAndAnswers(Question q, int questionNumber) {
+        int answerNumber = 0;
+        this.printQuestion(q.text(), questionNumber);
+        for (var a : q.answers()) {
+            this.printAnswer(a.text(), answerNumber++);
+        }
+        ioService.printLine("");
+    }
+
+    private void printAnswer(String text, int i) {
+        char letter = (char) ('a' + i);
+        ioService.printFormattedLine(ANSWER_SPACES + "%c) %s", letter, text);
+    }
+
+    private void printQuestion(String text, int questionNumber) {
+        ioService.printFormattedLine("%2d. %s", questionNumber, text);
     }
 }
