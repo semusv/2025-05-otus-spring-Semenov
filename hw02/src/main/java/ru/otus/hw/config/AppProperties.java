0a12a0b1df76d2f5a25ba9568918e5a5f3631b45
Semenov V <semvasek@mail.ru>
2025-06-11 21:10:20 +0300

    ready to do HW02

---
+++ hw02/src/main/java/ru/otus/hw/config/AppProperties.java
@@ -0,0 +1,17 @@
+package ru.otus.hw.config;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+
+public class AppProperties implements TestConfig, TestFileNameProvider {
+
+    // внедрить свойство из application.properties
+    private int rightAnswersCountToPass;
+
+    // внедрить свойство из application.properties
+    private String testFileName;
+}
