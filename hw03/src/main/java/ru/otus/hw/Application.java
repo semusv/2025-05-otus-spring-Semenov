8c040ef5739747633f67958bad8eda4448df83f4
Semenov V <semvasek@mail.ru>
2025-06-25 10:34:26 +0300

    Конфигурацию сделал через @EnableConfigurationProperties

--- hw03/src/main/java/ru/otus/hw/Application.java
+++ hw03/src/main/java/ru/otus/hw/Application.java
@@ -2,10 +2,13 @@ package ru.otus.hw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationContext;
+import ru.otus.hw.config.AppProperties;
 import ru.otus.hw.service.TestRunnerService;
 
 @SpringBootApplication
+@EnableConfigurationProperties(AppProperties.class)
 public class Application {
     public static void main(String[] args) {
 
--- hw03/src/main/java/ru/otus/hw/config/AppProperties.java
+++ hw03/src/main/java/ru/otus/hw/config/AppProperties.java
@@ -3,14 +3,12 @@ package ru.otus.hw.config;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
 
 import java.util.Locale;
 import java.util.Map;
 
 @Setter
 @ConfigurationProperties(prefix = "test")
-@Component
 public class AppProperties implements TestConfig, TestFileNameProvider, LocaleConfig {
 
     @Getter
