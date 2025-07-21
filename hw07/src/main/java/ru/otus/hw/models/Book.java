bf4d4e3fe7acf15446dd20fd438ea9f38ba1e958
Semenov V <semvasek@mail.ru>
2025-07-21 22:50:45 +0300

    убрал ненужные графы из сущностей

--- hw07/src/main/java/ru/otus/hw/models/Book.java
+++ hw07/src/main/java/ru/otus/hw/models/Book.java
@@ -11,8 +11,6 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.NamedAttributeNode;
-import jakarta.persistence.NamedEntityGraph;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -33,17 +31,6 @@ import java.util.List;
 @NoArgsConstructor
 @Entity
 @Table(name = "books")
-@NamedEntityGraph(
-        name = "book-author-entity-graph",
-        attributeNodes = {
-                @NamedAttributeNode("author"),
-        })
-@NamedEntityGraph(
-        name = "book-author-genres-entity-graph",
-        attributeNodes = {
-                @NamedAttributeNode("author"),
-                @NamedAttributeNode("genres")
-        })
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
