fad30eee8cf3730ff2f4199c96d29c896e7fca46
Semenov V <semvasek@mail.ru>
2025-09-21 17:24:54 +0300

    Использовал Spring Data REST Repository

--- hw16/src/main/java/ru/otus/hw/repositories/CommentRepository.java
+++ hw16/src/main/java/ru/otus/hw/repositories/CommentRepository.java
@@ -1,11 +1,15 @@
 package ru.otus.hw.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 import ru.otus.hw.models.Comment;
 
 import java.util.List;
 
+@RepositoryRestResource(path = "comment")
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    List<Comment> findByBookId(long id);
 
+    @RestResource(path = "bookId", rel = "bookId")
+    List<Comment> findByBookId(long id);
 }
